9f0f4810555cd010a704fbcf09d240d44193bdcd
tps <108142116+tps2022@users.noreply.github.com>
2022-08-25 08:52:36 -0400

    Greeting express application created

---
+++ server.js
@@ -0,0 +1,30 @@
+//initialize express
+const express = require('express');
+const app = express();
+const port = 3000
+const fs = require('fs') //require fs for hypatia view engine
+app.engine('hypatia', (filePath, options, callback) => { // define the view engine called hypatia
+  fs.readFile(filePath, (err, content) => {
+    if (err) return callback(err) // if error occurs
+    const rendered = content.toString()
+      .replace('#title#', '<title>' + options.title + '</title>')
+      .replace('#message#', '<h1>' + options.message + '</h1>').replace('#content#','<div>'+ options.content + '</div>' )
+    return callback(null, rendered)
+  })
+})
+app.set('views', './views') //set view folder
+app.set('view engine', 'hypatia') 
+
+
+app.get('/greeting',(req,res) => {
+    res.send('Hello, stranger')
+})
+
+app.get('/greeting/:name',(req,res) => {
+    res.send("What's up, " + req.params.name)
+})
+
+
+
+app.listen(3000, () => {
+    console.log("I am listening on port")
